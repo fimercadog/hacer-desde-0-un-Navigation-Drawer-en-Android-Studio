eb354c10304cfe1d96ef25eaf6d90274b7964718
fidel mercado <fimercadog@gmail.com>
2020-06-07 14:29:39 -0500

    commit initial

---
+++ app/src/main/java/net/mercado/fidel/hacer_desde_0_un_navigation_drawer_en_androidstudio/MainActivity.java
@@ -0,0 +1,14 @@
+package net.mercado.fidel.hacer_desde_0_un_navigation_drawer_en_androidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
