96dfcc8018c06da9b4f71d108c5898889a78aefe
fidel mercado <fimercadog@gmail.com>
2020-06-07 19:19:20 -0500

    commit so the menu considered and disapoars to the left, create calls fragment and setting manifest

---
+++ app/src/main/java/net/mercado/fidel/hacer_desde_0_un_navigation_drawer_en_androidstudio/Fragments/MainFragment.java
@@ -0,0 +1,6 @@
+package net.mercado.fidel.hacer_desde_0_un_navigation_drawer_en_androidstudio.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+}
--- app/src/main/java/net/mercado/fidel/hacer_desde_0_un_navigation_drawer_en_androidstudio/MainActivity.java
+++ app/src/main/java/net/mercado/fidel/hacer_desde_0_un_navigation_drawer_en_androidstudio/MainActivity.java
@@ -1,14 +1,42 @@
 package net.mercado.fidel.hacer_desde_0_un_navigation_drawer_en_androidstudio;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    //creacion de las variables
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //vinculacion de los xml con java
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout=findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navigationView);
+        navigationView=findViewById(R.id.navigationView);
+
+        //para que el menu aparezca y desapoaresca a la izquierda
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+
+
     }
 }
